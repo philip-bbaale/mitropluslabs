c5adafe481f5b6604625e4fc138fe5e9f0cedd5d
Philip <namugera.philip@gmail.com>
2023-11-02 14:29:21 +0300

    add div around children

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <Navbar></Navbar>
-        {children}
+        <div>{children}</div>
       </body>
     </html>
   );
