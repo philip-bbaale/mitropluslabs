09f01b45518c18109ad04820c78e4eb68a1d1bb9
Philip <namugera.philip@gmail.com>
2023-11-02 19:06:02 +0300

    re adjust and style nav bar

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -1,6 +1,11 @@
-"use client";
+'use client';
+import { useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import Logo from '/public/mitroplus_labs_logo.png';
 
 const links = [
+  // your links data
   {
     name: "Projects",
     href: "",
@@ -79,49 +84,92 @@ const links = [
   },
 ];
 
-import React from "react";
-import Link from "next/link";
-import Image from "next/image";
-import Logo from "/public/mitroplus_labs_logo.png"
-import { usePathname } from "next/navigation";
-
 export default function Navbar() {
-  return (
-    <nav className="flex justify-around flex-wrap items-center sticky top-0 backdrop-blur-xl bg-black opacity-90 p-3 z-50">
-      <div>
-        <Link href="/">
-        <Image src={Logo} alt="Mitroplus Labs Logo" height={30} quality={100} placeholder="blur" className="shadow-white"/>
-        </Link>
-      </div>
-      <div className="lg:flex space-x-4 md:hidden">
-        {links.map((link) => {
-          return (
-            <div
-              key={link.name}
-              href={link.href}
-              className="flex h-[48px] grow items-center justify-between gap-2 p-3 text-white text-sm font-medium cursor-context-menu hover:text-yellow-600 md:flex-none md:justify-start md:p-2 md:px-3"
-            >
-              <p className="hidden md:block">{link.name}</p>
-            </div>
-          );
-        })}
-      </div>
-      <button className=' inline-flex p-3 hover:bg-green-600 rounded lg:hidden text-white ml-auto hover:text-white outline-none'>
-          <svg
-            className='w-6 h-6'
-            fill='none'
-            stroke='currentColor'
-            viewBox='0 0 24 24'
-            xmlns='http://www.w3.org/2000/svg'
+    const [activeMenu, setActiveMenu] = useState(null);
+    const [isHamburgerOpen, setIsHamburgerOpen] = useState(false);
+  
+    return (
+      <nav className="fixed top-0 left-0 right-0 z-50 backdrop-blur-xl bg-opacity-90 bg-black p-3">
+        <div className={`flex justify-around items-center`}>
+          {/* Logo */}
+          <div className="flex items-center">
+            <Link href="/">
+              <Image
+                src={Logo}
+                alt="Mitroplus Labs Logo"
+                height={30}
+                quality={100}
+                placeholder="blur"
+                className="cursor-pointer"
+              />
+            </Link>
+          </div>
+  
+          {/* Hamburger Menu Button for Small Screens */}
+          <button
+            onClick={() => setIsHamburgerOpen(!isHamburgerOpen)}
+            className="md:hidden p-3 hover:bg-amber-600 rounded text-white hover:text-white outline-none"
           >
-            <path
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              strokeWidth={2}
-              d='M4 6h16M4 12h16M4 18h16'
-            />
-          </svg>
-        </button>
-    </nav>
-  );
-}
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+            </svg>
+          </button>
+  
+          {/* Large and Medium Screens Navigation Links */}
+          <div className={`hidden md:flex space-x-4`}>
+            {links.map((link, index) => (
+              <div
+                key={index}
+                className="relative group"
+                onMouseEnter={() => setActiveMenu(index)}
+                onMouseLeave={() => setActiveMenu(null)}
+              >
+                <div className="flex items-center p-3 text-white text-sm font-medium cursor-pointer group-hover:text-yellow-600">
+                  {link.name}
+                </div>
+                {activeMenu === index && (
+                  <div className="absolute top-full left-0 w-48 bg-white shadow-md z-50 py-2 space-y-2">
+                    {link.sub_links.map((subLink, subIndex) => (
+                      <a
+                        key={subIndex}
+                        href={subLink.href}
+                        className="block px-4 py-2 text-sm font-medium text-black hover:text-yellow-600"
+                      >
+                        {subLink.name}
+                      </a>
+                    ))}
+                  </div>
+                )}
+              </div>
+            ))}
+          </div>
+  
+          {/* Small Screen Navigation Links (Hamburger Menu Open) */}
+          {isHamburgerOpen && (
+            <div className="md:hidden fixed top-16 left-0 right-0 bg-white shadow-md p-4">
+              {links.map((link, index) => (
+                <div key={index} className="py-2">
+                  <p className="text-sm font-medium text-black">{link.name}</p>
+                  {link.sub_links.map((subLink, subIndex) => (
+                    <a
+                      key={subIndex}
+                      href={subLink.href}
+                      className="block py-1 text-sm font-medium text-black hover:text-yellow-600 pl-4"
+                    >
+                      {subLink.name}
+                    </a>
+                  ))}
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      </nav>
+    );
+  }
