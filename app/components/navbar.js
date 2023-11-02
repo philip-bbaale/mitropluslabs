17bfb987aeeaf97ae00282081e3e6ab5c1eb6947
Philip <namugera.philip@gmail.com>
2023-11-02 14:38:47 +0300

    add hambuger

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -93,7 +93,7 @@ export default function Navbar() {
         <Image src={Logo} alt="Mitroplus Labs Logo" height={30} quality={100} placeholder="blur" className="shadow-white"/>
         </Link>
       </div>
-      <div className="flex space-x-4">
+      <div className="lg:flex space-x-4 md:hidden">
         {links.map((link) => {
           return (
             <div
@@ -106,7 +106,22 @@ export default function Navbar() {
           );
         })}
       </div>
-      <div></div>
+      <button className=' inline-flex p-3 hover:bg-green-600 rounded lg:hidden text-white ml-auto hover:text-white outline-none'>
+          <svg
+            className='w-6 h-6'
+            fill='none'
+            stroke='currentColor'
+            viewBox='0 0 24 24'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth={2}
+              d='M4 6h16M4 12h16M4 18h16'
+            />
+          </svg>
+        </button>
     </nav>
   );
 }
