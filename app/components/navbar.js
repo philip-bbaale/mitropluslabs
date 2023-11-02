a6a3536caa5a41556526f4e836fe2e8361977b24
Philip <namugera.philip@gmail.com>
2023-11-02 12:51:32 +0300

    add navigation links data structure

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -1,9 +1,85 @@
-
+const links = [
+  {
+    name: "Projects",
+    href: "",
+    icon: "",
+    sub_links: [
+      {
+        name: "Literacy Centers",
+        href: "",
+        icon: "",
+      },
+      {
+        name: "Academy",
+        href: "",
+        icon: "",
+      },
+      {
+        name: "Market-mania",
+        href: "",
+        icon: "",
+      },
+      {
+        name: "Gorilla Hunt",
+        href: "",
+        icon: "",
+      },
+      {
+        name: "Gorilla Hub",
+        href: "",
+        icon: "",
+      },
+    ],
+  },
+  {
+    name: "Company",
+    href: "",
+    icon: "",
+    sub_links: [
+      {
+        name: "Founders",
+        href: "",
+        icon: "",
+      },
+      {
+        name: "The team",
+        href: "",
+        icon: "",
+      },
+      {
+        name: "Careers",
+        href: "",
+        icon: "",
+      },
+      {
+        name: "Publication",
+        href: "",
+        icon: "",
+      },
+    ],
+  },
+  {
+    name: "Safety",
+    href: "",
+    icon: "",
+    sub_links: [
+      {
+        name: "Charter",
+        href: "",
+        icon: "",
+      },
+      {
+        name: "Regulatory Aid",
+        href: "",
+        icon: "",
+      },
+    ],
+  },
+];
 
 export default function Navbar() {
-  return (
-    <nav>
-        
-    </nav>
-  )
+  return 
+  <nav>
+
+  </nav>;
 }
