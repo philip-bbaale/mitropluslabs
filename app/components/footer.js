33fe48f2f54f240ad1fab326504ed40fda3e268f
Philip <namugera.philip@gmail.com>
2023-11-02 20:58:43 +0300

    add footer fuctionality

--- app/components/footer.js
+++ app/components/footer.js
@@ -1,51 +1,122 @@
-import Link from 'next/link'
+"use client";
+import React from "react";
+import { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import Logo from "/public/mitroplus_labs_logo.png";
+
+const links = [
+  // your links data
+  {
+    name: "Projects",
+    href: "",
+    icon: "",
+    sub_links: [
+      {
+        name: "Literacy Centers",
+        href: "",
+        icon: "",
+      },
+      {
+        name: "Academy",
+        href: "",
+        icon: "",
+      },
+      {
+        name: "Market-mania",
+        href: "",
+        icon: "",
+      },
+      {
+        name: "Gorilla Hunt",
+        href: "",
+        icon: "",
+      },
+      {
+        name: "Gorilla Hub",
+        href: "",
+        icon: "",
+      },
+    ],
+  },
+  {
+    name: "Company",
+    href: "",
+    icon: "",
+    sub_links: [
+      {
+        name: "Founders",
+        href: "",
+        icon: "",
+      },
+      {
+        name: "The team",
+        href: "",
+        icon: "",
+      },
+      {
+        name: "Careers",
+        href: "",
+        icon: "",
+      },
+      {
+        name: "Publication",
+        href: "",
+        icon: "",
+      },
+    ],
+  },
+  {
+    name: "Safety",
+    href: "",
+    icon: "",
+    sub_links: [
+      {
+        name: "Charter",
+        href: "",
+        icon: "",
+      },
+      {
+        name: "Regulatory Aid",
+        href: "",
+        icon: "",
+      },
+    ],
+  },
+];
 
 export default function Footer() {
-  const footerItems = [
-    {
-      title: 'Projects',
-      links: ['Literacy Centers', 'Academy', 'Maketmania', 'Gorilla Hunt', 'Gorilla Lab']
-    },
-    {
-      title: 'Company',
-      links: ['Founders', 'The Team', 'Careers', 'Publications']
-    },
-    {
-      title: 'Safety',
-      links: ['Regulatory Aid', 'Charter']
-    },
-    {
-      title: 'Privacy Policy',
-      links: ['Terms & Conditions']
-    }
-  ]
+  const year = new Date().getFullYear();
 
   return (
-    <footer className="bg-white text-gray-600 body-font">
-      <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-        <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-          <span className="ml-3 text-xl">M</span>
-        </a>
-        <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2023 —
-          <a href="https://twitter.com/knyttneve" className="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@knyttneve</a>
-        </p>
-        <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-          {footerItems.map((item, index) => (
-            <div key={index} className="p-5 w-full">
-              <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">{item.title}</h2>
-              <nav className="list-none mb-10">
-                {item.links.map((link, linkIndex) => (
-                  <li key={linkIndex}>
-                    <Link href="#">
-                      <a className="text-gray-600 hover:text-gray-800">{link}</a>
-                    </Link>
-                  </li>
-                ))}
-              </nav>
-            </div>
-          ))}
-        </span>
+    <footer className="bg-white text-black py-6 px-4 lex flex-col flex-grow">
+      <div className="container mx-auto flex flex-wrap justify-between items-center">
+        <div className="w-full md:w-auto mb-4 md:mb-0 text-center md:text-left">
+          <Link href="/">
+            <Image
+              src={Logo}
+              alt="Mitroplus Labs Logo"
+              width={150}
+              height={50}
+            />
+          </Link>
+        </div>
+        {links.map((link, index) => (
+          <div key={index} className="w-full md:w-auto mb-4 md:mb-0">
+            <h6 className="font-semibold">{link.name}</h6>
+            <ul>
+              {link.sub_links.map((subLink, subIndex) => (
+                <li key={subIndex}>
+                  <Link href={subLink.href}>{subLink.name}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+      <div className="border-t mt-6 pt-6 text-center text-sm text-gray-600">
+        © {year} Mitroplus Labs
       </div>
     </footer>
-  )
+  );
 }
