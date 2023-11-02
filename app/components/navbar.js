67fe6e5c6e8134ebb4b4e3acd0cbb277f1d35fb8
Philip <namugera.philip@gmail.com>
2023-11-02 13:08:52 +0300

    use client side rendering add basic link map of nav links

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -1,3 +1,5 @@
+'use client';
+
 const links = [
   {
     name: "Projects",
@@ -77,9 +79,25 @@ const links = [
   },
 ];
 
-export default function Navbar() {
-  return 
-  <nav>
+import React from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
-  </nav>;
+export default function Navbar() {
+  return (
+    <nav>
+      {links.map((link) => {
+        return (
+          <Link
+            key={link.name}
+            href={link.href}
+            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+          >
+            <p className="hidden md:block">{link.name}</p>
+          </Link>
+        );
+      })}
+    </nav>
+  );
+  
 }
