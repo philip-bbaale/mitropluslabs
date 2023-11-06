314088e8b7ee808d431d9e044672b1ddd79416a0
Philip <namugera.philip@gmail.com>
2023-11-06 21:46:38 +0300

    add spline object

--- app/page.js
+++ app/page.js
@@ -1,14 +1,23 @@
-import Image from 'next/image'
-
-export const metadata = {
-  title: 'Mitroplus Labs',
-  description: 'Blockchain, Metaverse & Artificial Intelligence for Africa',
-}
+"use client";
+import Image from "next/image";
+import React, { Suspense } from "react";
+const Spline = React.lazy(() => import("@splinetool/react-spline"));
 
 export default function Home() {
   return (
     <main className=" container_box">
-      <h1 className='text-violet-400'>This is the Home page</h1>
+      <div className="grid-cols-12">
+        <div className="gird-cols-6">
+          <h1 className="text-violet-400">
+            Blockchain, Metaverse & Artificial Intelligence for Africa
+          </h1>
+        </div>
+        <div className="grid-cols-6">
+          <Suspense fallback={<div>Loading...</div>}>
+            <Spline scene="https://prod.spline.design/iWwNSLDMIpdH0Xmq/scene.splinecode" />
+          </Suspense>
+        </div>
+      </div>
     </main>
-  )
+  );
 }
