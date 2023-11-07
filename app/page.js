f040526291705292984f0d27cff1db4366204937
Philip <namugera.philip@gmail.com>
2023-11-07 21:48:01 +0300

    style header information

--- app/page.js
+++ app/page.js
@@ -1,21 +1,20 @@
 "use client";
 import Image from "next/image";
 import React, { Suspense } from "react";
-const Spline = React.lazy(() => import("@splinetool/react-spline"));
 
 export default function Home() {
   return (
-    <main className=" container_box">
-      <div className="grid-cols-12">
-        <div className="gird-cols-6">
-          <h1 className="text-violet-400">
-            Blockchain, Metaverse & Artificial Intelligence for Africa
+    <main className="container_box">
+      <div className=" flex justify-center ">
+        <div className="  lg:w-1/2 my-10">
+          <h1 className=" header1 text-center">
+            Blockchain, Metaverse & <br></br> Artificial Intelligence <br></br>{" "}
+            <span className=" text-yellow-600">for Africa</span>
           </h1>
-        </div>
-        <div className="grid-cols-6">
-          <Suspense fallback={<div>Loading...</div>}>
-            <Spline scene="https://prod.spline.design/iWwNSLDMIpdH0Xmq/scene.splinecode" />
-          </Suspense>
+          <p className="text2 text-center">
+            Bridging The Gap Between the New Emerging Digital world With the
+            Existing Mainstream Financial infrastructure and architecture.
+          </p>
         </div>
       </div>
     </main>
