de987fbef0cdab059ad7c69b77338f91b87497bd
Philip <namugera.philip@gmail.com>
2023-11-07 21:50:34 +0300

    add gap

--- app/page.js
+++ app/page.js
@@ -5,8 +5,8 @@ import React, { Suspense } from "react";
 export default function Home() {
   return (
     <main className="container_box">
-      <div className=" flex justify-center ">
-        <div className="  lg:w-1/2 my-10">
+      <div className=" flex justify-center gap-5">
+        <div className=" grid lg:w-1/2 my-10 gap-5">
           <h1 className=" header1 text-center">
             Blockchain, Metaverse & <br></br> Artificial Intelligence <br></br>{" "}
             <span className=" text-yellow-600">for Africa</span>
