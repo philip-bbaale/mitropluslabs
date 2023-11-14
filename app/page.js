5f944bf0a28f2e3ca47f8ac33e8db02fcaa0be91
Philip <namugera.philip@gmail.com>
2023-11-14 13:49:31 +0300

    import particles component

--- app/page.js
+++ app/page.js
@@ -1,11 +1,14 @@
 "use client";
 import Image from "next/image";
 import React, { Suspense } from "react";
+import ParticlesBackground from "./components/ParticlesBackground";
 
 export default function Home() {
   return (
     <main className="container_box">
+
       <div className=" flex justify-center gap-5">
+      <ParticlesBackground />
         <div className=" grid lg:w-1/2 my-10 gap-5">
           <h1 className=" header1 text-center">
             Blockchain, Metaverse & <br></br> Artificial Intelligence <br></br>{" "}
@@ -17,6 +20,7 @@ export default function Home() {
           </p>
         </div>
       </div>
+
     </main>
   );
 }
